b3469b0d4bd5dcbad19ef4a8ac8b8f57e1cde46a
Shinjinok <bartshin00@gmail.com>
2021-09-05 23:43:42 +0900

    octave

--- impedence.m
+++ impedence.m
@@ -1,6 +1,5 @@
-clear all
-close all
 
+clear; clc; close all;
 
 R1m = 6.8;
 P0 = 67.2;
@@ -42,99 +41,93 @@ Pc = P0 - Psl - Prl -PM;
 %Zb = 1/(1/0.5j*Xm + 1/(0.5*R2/(2-s) + 0.5j*X2))
 
 
-Rc = 0;
+%Rc = 0;
 %R2 = 1
 %X1m = 1
-Xm = 1;
+Xm = X1m;
 %X2 = 1;
-k = 1;
-X = [R1m R2 X1m Xm X2]';
-
-for i=1:5,
-
-R1m = X(1);
-R2 = X(2);
-X1m = X(3);
-Xm = X(4);
-X2 = X(5);
+X1a = X1m;
 
 
+syms f1(r1m, r2, x1a, x1m, x2, xm);
+syms f2(r1m, r2, x1a, x1m, x2, xm);
+syms f3(r1m, r2, x1a, x1m, x2, xm);
+syms f4(r1m, r2, x1a, x1m, x2, xm);
+syms f5(r1m, r2, x1a, x1m, x2, xm);
+syms f6(r1m, r2, x1a, x1m, x2, xm);
 
-R0 = R1m + 0.25*R2;
-X0 = X1m + 0.5 * Xm + 0.5* X2;
-Z0 = R0 + j*X0;
+z0_re = r1m + r2/4;
+z0_im = x1m + xm/2 + x2/2;
+%zs_re = r1m + r2*xm^2 / (r2^2+(x2+xm)^2);
+%zs_im = x1m + (r2^2*xm+xm*x2^2+xm^2*x2) / (r2^2+(x2+xm)^2);
+zs_re = r1m + r2;
+zs_im = x1m + x2;
+zsa_re = R1a + r2;
+zsa_im = x1a + x2;
 
-Rs = R1m + R2;
-Xs = X1m + X2 ;
-Zs = Rs + j*Xs;
-
-Zs2 = R1m + j*X1m + 1/(1/(j*Xm) + 1/(R2+j*X2));
-
-
-
-J = [0 0 1 0 -1
-    -R0/abs(Z0) -0.25*R0/abs(Z0) -X0/abs(Z0) -0.5*X0/abs(Z0) -0.5*X0/abs(Z0)
-    -I0^2 -0.25*I0^2  0 0 0
-    -Rs/abs(Zs) -Rs/abs(Zs) -Xs/abs(Zs) 0 -Xs/abs(Zs)
-    -Is^2 -Is^2 0 0 0];
-
-Psl = R1m*I0^2;
-Prl = 0.25*R2*I0^2;
-Pc = P0 - Psl - Prl -PM;
+f1 = x1m - x2;
+f2 = V0/I0 - sqrt(z0_re^2 + z0_im^2);
+f3 = Vsa/Isa - sqrt(zsa_re^2 + zsa_im^2);
+f4 = P0-PM-Pc-z0_re*I0^2;
+f5 = Vs/Is - sqrt(zs_re^2 + zs_im^2);
+f6 = Ps - zs_re*Is^2;
 
 
-Y = [ X1m-X2
-      V0/I0-abs(Z0)
-      P0-PM-Pc-real(Z0)*I0^2
-      Vs/Is-abs(Zs)
-      Ps-real(Zs)*Is^2];
+jf = jacobian([f1;f2;f3;f4;f5;f6]);
+djf = function_handle(jf);
+f = [f1;f2;f3;f4;f5;f6];
+ff =function_handle(f);
 
+k=1;
+X = [R1m R2 X1a X1m X2 Xm]';
+
+for i=1:10,
+Y = ff(X(1), X(2), X(3), X(4),X(5), X(6));
+J = djf(X(1), X(2), X(3), X(4),X(5), X(6));
 X = X - k*inv(J)*Y;
-elog(i,:) = Y;
 end
 
-
-X1a = X1m;
-for i=1:10,
-Rsa = R1a + R2;
-Xsa = X1a + X2 ;
-Zsa= Rsa + j*Xsa;
-
-Ya = Vsa/Isa - abs(Zsa);
-Ja =  - Xsa/abs(Zsa);
-X1a = X1a - inv(Ja)*Ya;
-
-end;
-R1m
-R1a
-R2
-Xm
-X1m
-X1a
-X2
+disp( 'Motor parameter')
+R1a 
+R1m = X(1)
+R2 = X(2)
+X1a = X(3)
+X1m = X(4)
+X2 = X(5)
+Xm = X(6)
 PM
 Pc
-Psl
-Prl
+%%Zs2 = R1m + j*X1m + 2/(1/(j*0.5*Xm) + 1/(0.5*R2+j*0.5*X2))
+%zre = R1m + R2*Xm^2 / (R2^2+(X2+Xm)^2)
+%zim = X1m + (R2^2*Xm+Xm*X2^2+Xm^2*X2) / (R2^2+(X2+Xm)^2)
+disp( 'turn ratio')
 a = sqrt(X1a/X1m)
-
+disp( '----------------------')
 f = 60
-Rpm = 1780
+Rpm = 1750
 R = 120*f/4
 s=(R-Rpm)/R
 w = 2*3.14*f
 C = 30*1e-6
 Xc = 1/(w*C)
+Vm = 120;
 
 Z1m = R1m+j*X1m;
 Zf  = R1m + j*X1m + 1/(1/(j*0.5*Xm) + 1/(0.5*R2/s+j*0.5*X2));
-Zb  = R1m + j*X1m + 1/(1/(j*0.5*Xm) + 1/(0.5*R2/(1-s)+j*0.5*X2));
-Zc = j*Xc;
+Zb  = R1m + j*X1m + 1/(1/(j*0.5*Xm) + 1/(0.5*R2/(2-s)+j*0.5*X2));
+Zc = -j*Xc;
 Z1a = R1a+j*X1a;
 Z11 = Z1m+Zf+Zb;
 Z12 = -j*a*(Zf-Zb);
 Z21 = j*a*(Zf-Zb);
 Z22 = Zc+Z1a+a^2*(Zf+Zb);
 Z_m = [Z11 Z12; Z21 Z22]
-Iin = inv(Z_m)*[120 120]'
-Iin2 = abs(Iin(1)+Iin(2))
+disp('current')
+Ima = inv(Z_m)*[Vm Vm]';
+Iin = Ima(1) + Ima(2)
+pf = real(Iin)/abs(Iin)
+Pin = Vm*abs(Iin)*pf
+
+
+
+
